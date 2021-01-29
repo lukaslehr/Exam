0845b4998513cebe9c9bf3101c96d69d031dcc4b
Lukas <lule@itu.dk>
2021-01-29 23:29:54 +0100

    Setup

---
+++ console-app/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using library;
+
+namespace console_app
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Question.Hello("Brian");
+        }
+    }
+}
---
+++ library/Wagon.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace library
+{
+
+
+    public class Wagon
+    {
+        public int Id { get; set; }
+
+
+        [Required]
+        [StringLength(20)]
+        public string Name { get; set; }
+
+        public int LocomotiveID { get; set; }
+
+        #nullable enable
+        public Locomotive? Locomotive { get; set; }
+
+    }
+
+    public class Locomotive
+    {
+    }
+}
