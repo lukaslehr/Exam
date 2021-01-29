d97883c3c7d0782dee05300aa275d3e1139409ef
Lukas <lule@itu.dk>
2021-01-30 00:30:18 +0100

    Gitignore

--- console-app/Program.cs
+++ console-app/Program.cs
@@ -7,7 +7,12 @@ namespace console_app
     {
         static void Main(string[] args)
         {
+<<<<<<< Updated upstream
             Question.Hello("Brian");
+=======
+            var q = new Question();
+            Console.WriteLine(q.Largest(2.4f, 4.5f));
+>>>>>>> Stashed changes
         }
     }
 }
